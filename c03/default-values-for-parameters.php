742ceb2917059eb1d8b2fa0abd35f48712488474
pmenezes3094 <pmene.nic@gmail.com>
2023-09-26 08:14:29 -0700

    Change the discount to 7

--- c03/default-values-for-parameters.php
+++ c03/default-values-for-parameters.php
@@ -14,7 +14,7 @@ function calculate_cost($cost, $quantity, $discount = 2)
   <body>
     <h1>The Candy Store</h1>
     <h2>Chocolates</h2>
-    <p>Dark chocolate $<?= calculate_cost(5, 10, 5) ?></p>
+    <p>Dark chocolate $<?= calculate_cost(5, 10, 7) ?></p>
     <p>Milk chocolate $<?= calculate_cost(3, 4) ?></p>
     <p>White chocolate $<?= calculate_cost(4, 15, 20) ?></p>
   </body>
